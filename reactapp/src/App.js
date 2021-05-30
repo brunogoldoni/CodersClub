f6fa25f48663d8e02f939638b86ddb5f5deffda2
bruno <bgoldoni@blupay.com.br>
2021-05-30 16:33:51 -0300

    State

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,21 @@
-import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  function handleCounter() {
+    setTimeout(() => {
+      setCounter((oldCounter) => oldCounter + 1);
+    }, 2000);
+  }
+
   return (
     <div>
       <h1>Hello React App</h1>
-
-      <Button>Cadastrar</Button>
+      Contador: {counter}
+      <div>
+        <button onClick={handleCounter}>Soma Contador</button>
+      </div>
     </div>
   );
 }
