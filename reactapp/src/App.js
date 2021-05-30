9716a9289057b8a887bf7866280df705a1dd167b
bruno <bgoldoni@blupay.com.br>
2021-05-30 19:01:28 -0300

    Request
    -HTTP
    -Axios

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,30 @@
-import List from "./components/List";
+import { useState } from "react";
+import api from "./services/api";
 
 function App() {
+  const [username, setUsername] = useState("brunogoldoni");
+  const [userData, setUserData] = useState({});
+
+  async function getUserGithubData() {
+    const { data } = await api.get(username);
+
+    setUserData(data);
+  }
+
   return (
     <div>
       <h1>Hello React App</h1>
 
       <div>
-        <List />
+        <input
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+
+        <button onClick={getUserGithubData}>Pesquisar usuário</button>
+        <div>{userData.name}</div>
+        <div>{userData.company}</div>
       </div>
     </div>
   );
