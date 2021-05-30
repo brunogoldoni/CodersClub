ec993db013a5ef41d88f0eb81e1879945856c094
bruno <bgoldoni@blupay.com.br>
2021-05-30 18:17:43 -0300

    Hooks
    -useCallback
    -useMemo

--- reactapp/src/components/List/index.js
+++ reactapp/src/components/List/index.js
@@ -1,19 +1,25 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
+
+const set = new Set();
 
 function List() {
   const [counter, setCounter] = useState(1);
 
-  useEffect(() => {
-    console.log("Atualizou...");
-  }, [counter]);
+  const handleSetCounter = useCallback(() => {
+    setCounter((oldCounter) => oldCounter + 1);
+    set.add(handleSetCounter);
+  }, []);
+
+  console.log("COUNTER", set.size);
+
+  // const number = useMemo(() => 1234588 * 9763, []);
+  // console.log("NUMBER", number);
 
   return (
     <>
       <div>Contador: {counter}</div>
       <div>
-        <button onClick={() => setCounter((oldCounter) => oldCounter + 1)}>
-          Soma Contador
-        </button>
+        <button onClick={handleSetCounter}>Soma Contador</button>
       </div>
     </>
   );
